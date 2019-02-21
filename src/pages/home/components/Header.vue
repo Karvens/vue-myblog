dbf500de1b88677b1060cc4acff74226f14910af
pangqihao <1742211396@qq.com>
2019-02-21 17:57:47 +0800

    xxxx

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+ <div class="header">
+   <div class="header-title"></div>
+   <div class="header-blog">KARVENS</div>
+   <div class="header-link"></div>
+   <div class="header-Archive"></div>
+   <div class="header-message"></div>
+   <div class="header-about"></div>
+ </div>
+y
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+<style lang="stylus" scoped>
+  .header
+     height: 70px
+     background-color: #42b983
+    .header-title
+       font-size: 22
+       font-family: Avenir
+       line-height: 22px
+       text-align: center
+       color: #25a4bb
+       margin-left 100px
+</style>
